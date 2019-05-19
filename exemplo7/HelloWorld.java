dc22d47bcd516e070efa9e5551966edc67e3ac6c
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-19 09:23:14 -0300

    exemplo 7

---
+++ exemplo7/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (String name : args) {
+            System.out.println("Hello " + name);
+        }
+    }
+}
---
+++ exemplo7/readme.md
@@ -0,0 +1,12 @@
+# Exemplo 7
+
+Explorando a possibilidade de argumentos.
+
+Compilando
+
+    javac HelloWorld
+
+Execuntado A classe com argumentos
+
+    java HelloWorld Tom Dick harry
+
