3567e75504a32974f529ea192917b05c7c6d8135
Flávio Micheletti <famicheletti@indracompany.com>
2019-05-19 09:23:06 -0300

    exemplo 6

---
+++ exemplo6/Trial.java
@@ -0,0 +1,13 @@
+class ForGeeks
+{
+   public static void main(String[] args){
+      System.out.println("ForGeeks class");
+   }
+}
+
+class ForDummies
+{
+   public static void main(String[] args){
+      System.out.println("ForDummies class");
+   }
+}
---
+++ exemplo6/readme.md
@@ -0,0 +1,12 @@
+# Exemplo 6
+
+Duas classes, dois métodos `main`, pode isso Aranaldo ?
+
+    # compilar
+    javac Trial.java
+
+
+Ao executar, você deve escolher a classe que será acionada.
+
+    java ForGeeks
+    java ForDummies
